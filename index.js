5e8c74ba1dd7dd7e167818a7b902173193d362b6
runslow917 <connectjeevanhegde@gmail.com>
2017-12-16 12:31:30 +0530

    added index info

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("index.js is executed");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './src/index.css';
+import App from './src/App';
+import registerServiceWorker from './src/registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
